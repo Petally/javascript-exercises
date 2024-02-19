d7893687f181941ae64673423b2ece71ab505091
Davids Levs Zvirgzds <davids.levs@gmail.com>
2024-02-19 10:04:20 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(range1, range2) {
+  if (range1 < 0 || range2 < 0) { return "ERROR"; }
+  if (typeof range1 !== 'number' || typeof range2 !== 'number') { return "ERROR"; }
 
+  const start = (range1 < range2) ? range1 : range2;
+  const end = (range1 < range2) ? range2 : range1; 
+  
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
