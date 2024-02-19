27ae016fa267d82bd08e9ff16c3cb8d852f054f1
Davids Levs Zvirgzds <davids.levs@gmail.com>
2024-02-19 09:12:48 +0200

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeatAmount) {
+  if (Math.sign(repeatAmount) === -1) return "ERROR";
 
+  let repeatedString = "";
+  
+  for (let i = 0; i < repeatAmount; i++) {
+    repeatedString += string;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
