7d774d6ba252e62c2ed9c0b6b463b9074da7ed62
Petally <petalimium@gmail.com>
2024-03-04 23:24:14 +0200

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let processed = parseInt(index);
+    if (processed === 0) { return 0; }
+    if (processed < 0) {
+        return "OOPS";
+    } else {
+        let a = 0;
+        let b = 1;
+        let c = 0;
+        for (let i = 2; i <= processed; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
